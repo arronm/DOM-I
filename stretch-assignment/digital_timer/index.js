b5fe539fa3d4175d4f1205c9ec8a5543742b693c
Arron M <marshall.arron@gmail.com>
2019-04-08 13:01:28 -0700

    Add initial start timer button

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,11 +1,21 @@
 const digits = document.querySelector('.digits');
 
 let counter = 0;
-const timer = window.setInterval(() => {
-  counter += 10;
-  digits.querySelector('#secondTens').innerHTML = Math.floor(counter / 10000);
-  digits.querySelector('#secondOnes').innerHTML = Math.floor(counter / 1000) % 10;
-  digits.querySelector('#msHundreds').innerHTML = Math.floor(counter / 100) % 10;
-  digits.querySelector('#msTens').innerHTML = Math.floor(counter / 10) % 10;
-  if (counter === 10000) clearInterval(timer);
-}, 10);
+const startTimer = () => {
+  let timer = window.setInterval(() => {
+    counter += 10;
+    digits.querySelector('#secondTens').innerHTML = Math.floor(counter / 10000);
+    digits.querySelector('#secondOnes').innerHTML = Math.floor(counter / 1000) % 10;
+    digits.querySelector('#msHundreds').innerHTML = Math.floor(counter / 100) % 10;
+    digits.querySelector('#msTens').innerHTML = Math.floor(counter / 10) % 10;
+    if (counter === 10000) {
+      digits.style.color = 'red';
+      clearInterval(timer);
+    }
+  }, 10);
+};
+
+document.addEventListener('click', (event) => {
+  if (!event.target.matches('#start-timer')) return;
+  startTimer();
+});
